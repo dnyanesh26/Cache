929fa2a7f14add3a229627085f73177685122bd7
Dnyanesh <dhumal.dnyanesh26@gmail.com>
2025-06-23 22:54:42 +0530

    controller to dao added

---
+++ src/main/java/com/JWT/Cache/controller/UserController.java
@@ -0,0 +1,112 @@
+package com.JWT.Cache.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings("unused")
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/user-api")
+public class UserController {
+	
+	@Autowired
+	UserService service;
+	
+	
+	
+//	@GetMapping("/getEmpbyId/{eid}")
+//	public Employee getEmpbyId(@PathVariable("eid") int userId)
+//	{
+//		Employee e=service.getEmpbyId(userId);
+//		return e;
+//	}
+//	
+//	
+//	@GetMapping("/employee")
+//	public List<Employee> getEmpList()
+//	{
+//		return(service.getEmpList());
+//	}
+//	
+	@PostMapping(path="/addUser")
+	public String addUser(@RequestBody User user) 
+	{	System.out.println(user);
+		
+		try {
+			return("User "+service.addUser(user)+" added");
+		} catch (UserException e) {
+			// TODO Auto-generated catch block
+			
+			return(e.getMessage());
+		}
+	}
+	
+	// @PostMapping("/user")
+	// public @ResponseBody User getUser(@RequestBody LinkedHashMap u) throws UserException
+	// {
+		
+	// 	String email=(String)u.get("email");
+	// 	System.out.println(email);
+	// 	return service.getUser(email);
+	// }
+	
+	// @PutMapping("/updateUser")
+	// public @ResponseBody User updateUser(@RequestBody User user)
+	// {
+	// 	return service.updateUser(user);
+	// }
+	
+	// @DeleteMapping("/deleteUser")
+	// public String deleteUser(@RequestBody LinkedHashMap u)
+	// {
+	// 	int Id=Integer.parseInt((String)u.get("Id"));
+	// 	return service.deleteUser(Id);
+	// }
+	
+	// @PostMapping("/policy")
+	// public List<Policy> getPolicy(@RequestBody LinkedHashMap u)
+	// {
+	// 	int Id=Integer.parseInt((String)u.get("Id"));
+	// 	return service.getPolicy(Id);
+	// }
+	
+//
+//	@PutMapping("/editSal")
+//	public Employee editSal(@RequestBody Employee e)
+//	
+//	{
+//		
+//		return (service.editSal(e.getEmpNo(),e.getEmpSal()));
+//	}
+//	
+//	@PostMapping("/addempdept/{did}")
+//	public int addEmpDept(@PathVariable("did") int Id,@RequestBody Employee e)
+//	{
+//		Department d = deptservice.getDeptbyId(Id);
+//		e.setDepartment(d);
+//		
+//		return(service.addEmployee(e));
+//		
+//	}
+//	@PostMapping("/addempdept")
+//	public int addEmpDept(@RequestBody Employee e)
+//	{
+//		
+//		
+//		return(service.addEmployee(e));
+//		
+//	}
+	
+	
+}
---
+++ src/main/java/com/JWT/Cache/entity/User.java
@@ -0,0 +1,136 @@
+package com.JWT.Cache.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="USERS")
+public class User {
+	@Id
+	@GeneratedValue(strategy =GenerationType.SEQUENCE,generator="USERIDSEQ")
+	@SequenceGenerator(name="USERIDSEQ",sequenceName="useridseq",allocationSize = 1,initialValue = 1)
+	@Column(name="USERID")
+	private int userId;
+	
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="EMAIL",unique = true)
+	private String email;
+	
+	@Column(name="DOB")
+	private Date dob;
+	
+	@Column(name="CONTACTNO",unique=true)
+	private String contactNo;
+	
+	@Column(name="ADDRESS")
+	private String address;
+	
+	@Column(name="PASSWORD")
+	private String password;
+	
+	// @OneToMany(mappedBy="user")
+	// private List<Vehicle> vehicleList;
+	
+	// @OneToMany(mappedBy="user")
+	// private List<Ticket> ticketList;
+	
+	// @OneToMany(cascade=CascadeType.ALL,mappedBy="user")
+	// private List<Policy> policyList;
+	
+	// @OneToMany(cascade=CascadeType.ALL,mappedBy="user")
+	// private List<Claim> claimList;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int userId, String name, String email, Date dob, String contactNo, String address, String password) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.contactNo = contactNo;
+		this.address = address;
+		this.password = password;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", name=" + name + ", email=" + email + ", dob=" + dob + ", contactNo="
+				+ contactNo + ", address=" + address + ", password=" + password + "]";
+	}
+	
+	
+
+}
